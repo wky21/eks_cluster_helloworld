93b982c01b597b992d7c87a947ca6ccd02a15d20
wky21 <wongsta92@gmail.com>
2025-12-16 00:49:31 -0800

    update eks module version

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,7 +1,7 @@
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.32.0"
+  version = "~> 20.34.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
