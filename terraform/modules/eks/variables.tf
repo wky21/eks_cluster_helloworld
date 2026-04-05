9e259eb0b20926b0fdcc0085c2d681a7bd5bc44b
wky21 <wongsta92@gmail.com>
2026-04-04 23:23:05 -0700

    updated EKS cluster version because v1.29 is out of support

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -7,7 +7,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.29"
+  default     = "1.34"
 }
 
 variable "vpc_id" {
